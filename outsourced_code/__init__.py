10d76c0abf633d6d1b93d852ae66743cc0f09d26
svenbuder <sven.buder@anu.edu.au>
2024-09-19 09:58:16 +1000

    cleaning up some code outsourcing cells. reformatting some plot_clinic_plots

--- outsourced_code/__init__.py
+++
@@ -1,7 +0,0 @@
-from .spectrum_reader import read_spectrum
-from .spectrum_normalizer import normalize_spectrum
-from .radial_velocity_corrector import correct_radial_velocity
-from .gaussian_fitter import fit_gaussian, gaussian
-
-# Optional: Define an __all__ variable to control what's exported by default
-__all__ = ['read_spectrum', 'normalize_spectrum', 'correct_radial_velocity', 'fit_gaussian']
